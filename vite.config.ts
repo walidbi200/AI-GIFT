ec97e7b355b14af8278d48e437cb1497a36b4ab8
walidbichri <bichriwalid1@gmail.com>
2025-07-12 18:02:54 +0100

    SEO optimized

--- vite.config.ts
+++ vite.config.ts
@@ -30,4 +30,16 @@ export default defineConfig({
       }
     })
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
